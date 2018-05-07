9746262ed9aeaf76138d0878ac0235b530c9b2e2
Shidfar Hodizoda <shidfarhf@yahoo.com>
2018-05-07 21:08:54 +0800

    semicolons

--- src/lib/db-wrapper.ts
+++ src/lib/db-wrapper.ts
@@ -10,24 +10,24 @@ export function Client(context: Context): DbWrapper {
         user     : 'root',
         password : '1234',
         database : 'restaurant_recommender_db'
-    });
+    })
 
     function insertInto(table: string, values: any): Promise<any> {
         return new Promise((resolve, reject) => {
             pool.getConnection(function(err, connection) {
                 if (err) {
-                    console.log('Could not get connection from the pool.', err);
-                    return reject(err);
+                    console.log('Could not get connection from the pool.', err)
+                    return reject(err)
                 }
                 connection.query(`INSERT INTO ${table} SET ?`, values, (error, results, fields) => {
-                    connection.release();
+                    connection.release()
                     if (error) {
-                        console.log('Could not query.', error, results);
+                        console.log('Could not query.', error, results)
                         return reject(error)
                     }
                     return resolve({ results, fields })
                 })
-            });
+            })
         })
     }
 
@@ -35,18 +35,18 @@ export function Client(context: Context): DbWrapper {
         return new Promise((resolve, reject) => {
             pool.getConnection(function(err, connection) {
                 if (err) {
-                    console.log('Could not get connection from the pool.', err);
-                    return reject(err);
+                    console.log('Could not get connection from the pool.', err)
+                    return reject(err)
                 }
                 connection.query(`SELECT * FROM restaurants`, (error, results, fields) => {
-                    connection.release();
+                    connection.release()
                     if (error) {
-                        console.log('Could not query.', error, results);
+                        console.log('Could not query.', error, results)
                         return reject(error)
                     }
                     return resolve({ results, fields })
                 })
-            });
+            })
         })
     }
 
