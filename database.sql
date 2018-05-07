8b5842a98b44b95fc9e9477d33b4d294101b9bb4
Shidfar Hodizoda <shidfarhf@yahoo.com>
2018-05-07 20:29:19 +0800

    continue

--- database.sql
+++ database.sql
@@ -5,7 +5,7 @@ CREATE TABLE IF NOT EXISTS users (
     id INT(9) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
     email VARCHAR(255) NOT NULL,
     password VARCHAR(255) NOT NULL,
-    phone VARCHAR(255) NOT NULL,
+    phone VARCHAR(18),
     address VARCHAR(255) NOT NULL,
     registration_date TIMESTAMP
 );
--- scripts/migrator.js
+++ scripts/migrator.js
@@ -33,26 +33,26 @@ const restaurants = require('../data/restaurants-list.json');
 const users = require('../data/users.json');
 
 (async () => {
-    for (user of users) {
-        const { email, password, phone, address } = user
-        const values = { email, password, phone, address }
-        await insertData('users', values)
-    }
-
-    let c = 1;
-    for (restaurant of restaurants.restaurantList) {
-        const { name } = restaurant
-        const values = { name }
-        await insertData('restaurants', values)
-        await insertData('restaurant_menu', { restaurant_id: c })
-        c++;
-    }
+    // for (user of users) {
+    //     const { email, password, phone, address } = user
+    //     const values = { email, password, phone, address }
+    //     await insertData('users', values)
+    // }
+    //
+    // let c = 1;
+    // for (restaurant of restaurants.restaurantList) {
+    //     const { name } = restaurant
+    //     const values = { name }
+    //     await insertData('restaurants', values)
+    //     await insertData('restaurant_menu', { restaurant_id: c })
+    //     c++;
+    // }
 
     let { name, workingHours: working_hours, address, cuisine, image, rating } = descs11
 
-    await insertData('restaurant_descriptions', { name, working_hours, address, cuisine, image, rating })
+    await insertData('restaurant_descriptions', { restaurant_id: 1, name, working_hours, address, cuisine, image, rating })
     ({ name, workingHours: working_hours, address, cuisine, image, rating } = descs12)
-    await insertData('restaurant_descriptions', { name, working_hours, address, cuisine, image, rating })
+    await insertData('restaurant_descriptions', { restaurant_id: 2, name, working_hours, address, cuisine, image, rating })
 
     for (menu of menu11.menuList) {
         const { nameFood: name, description, price } = menu
